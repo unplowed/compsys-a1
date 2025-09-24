c1b293c56a50acb70403355fe33a5d3a187f92da
spoody <m1nix@mail.spoodythe.one>
2025-09-24 17:42:34 +0200

    indexed id query

---
+++ id_query_indexed.c
@@ -0,0 +1,59 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include "id_query.h"
+#include "record.h"
+
+struct IndexRecord {
+  int64_t osm_id;
+  const struct Record *record;
+};
+
+// sized array
+struct IndexedData {
+  struct IndexRecord *records;
+  int length;
+};
+
+struct IndexedData *mk_indexed(struct Record *records, int length) {
+  struct IndexedData *data = malloc(sizeof(struct IndexedData));
+  struct IndexRecord *list = malloc(sizeof(struct IndexRecord) * length);
+
+  for (int i = 0; i < length; i++) {
+    struct IndexRecord record;
+    record.osm_id = records[i].osm_id;
+    record.record = &records[i];
+    list[i] = record;
+  }
+
+  data->records = list;
+  data->length = length;
+
+  return data;
+}
+
+void free_indexed(struct IndexedData *data) {
+  for (int i = 0; i < data->length; i++) {
+    free((void *)data->records[i].record);
+    free(&data->records[i]);
+  }
+  free(data);
+}
+
+const struct Record *lookup_indexed(struct IndexedData *data, int64_t needle) {
+  for (int i = 0; i < data->length; i++) {
+    if (data->records[i].osm_id == needle)
+      return data->records[i].record;
+  }
+  return NULL;
+}
+
+int main(int argc, char **argv) {
+  return id_query_loop(argc, argv, (mk_index_fn)mk_indexed,
+                       (free_index_fn)free_indexed, (lookup_fn)lookup_indexed);
+}
